d922bf55d83c0b291d807aa917b758ccb3b5d7ca
Elliot Winard <enw@caveteen.com>
2014-05-11 18:57:37 -0700

    comment, externalize time before showing second image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 $(function ($) {
-    function changeImage(eh) {
-      $("body").css("background-image", "url('/img/MD2014_1.png')")
+    // timeout ms
+    var TIMEOUT_MS = 10 * 1000; // 10 sec
+
+    // images to show
+    var imageArr = [
+        "url('/img/MD2014_0.png')",
+        "url('/img/MD2014_1.png')"
+    ];
+
+    // function generator to show a specific image
+    function getShowImageFxn(num) {
+        return function () {
+            $("body").css("background-image", imageArr[num])
+        };
     }
-    setTimeout(changeImage, 2000);
+
+    // show second image eventually
+    setTimeout(getShowImageFxn(1), TIMEOUT_MS);
   })
